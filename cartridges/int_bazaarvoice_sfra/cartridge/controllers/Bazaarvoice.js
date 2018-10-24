61bad586973ee4d157239c4813f83209551311c0
Brendan Smith <bsmith2@degdigital.com>
2018-10-23 22:57:13 -0400

    container page controller and template

---
+++ cartridges/int_bazaarvoice_sfra/cartridge/controllers/Bazaarvoice.js
@@ -0,0 +1,31 @@
+/**
+* Bazaarvoice.js
+* 
+* Used to host the Bazaarvoice Container page.
+*
+* @module  controllers/Bazaarvoice
+*/
+
+'use strict';
+
+var server = require('server');
+
+/**
+* Container
+* 
+* Simply loads the bv.js scout file on a blank page.  Used by Bazaarvoice as a review submission container for mobile
+* reviews and non-mobile reviews from emails.
+*
+* @return {String} The string 'myFunction'
+*/
+server.get('Container', function(req, res, next) {
+	var BVHelper = require('int_bazaarvoice_sfra/cartridge/scripts/lib/libBazaarvoice').getBazaarVoiceHelper();
+	
+	res.render('bv/container/container', {
+		bvScout: BVHelper.getBvLoaderUrl()
+	});
+	
+	next();
+});
+
+module.exports = server.exports();
